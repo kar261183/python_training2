404bf9601fb6bd922b0fab71d660130903ec00aa
Kar Tka <kar.tkachenko@gmail.com>
2022-11-09 22:29:42 -0800

    extracting and parameters

--- test_2.py
+++ test_2.py
@@ -12,31 +12,59 @@ class UntitledTestCase(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-    def test_untitled_test_case(self):
+    def test_create_group(self):
         driver = self.driver
-        driver.get("http://localhost/addressbook")
-        driver.find_element(By.NAME, "user").click()
-        driver.find_element(By.NAME, "user").clear()
-        driver.find_element(By.NAME, "user").send_keys("admin")
-        driver.find_element(By.ID, "LoginForm").click()
-        driver.find_element(By.NAME, "pass").click()
-        driver.find_element(By.NAME, "pass").clear()
-        driver.find_element(By.NAME, "pass").send_keys("secret")
-        driver.find_element(By.XPATH, "//input[@value='Login']").click()
-        driver.find_element(By.LINK_TEXT, "groups").click()
+        self.open_home_page(driver)
+        self.login(driver, username="admin", password="secret")
+        self.open_group_page(driver)
+        self.create_new_group(driver, name="12345", header="rrrrrr", footer="l;okljkhkh")
+        self.return_to_group_page(driver)
+        self.logout(driver)
+
+    def test_create_empty_group(self):
+        driver = self.driver
+        self.open_home_page(driver)
+        self.login(driver, username="admin", password="secret")
+        self.open_group_page(driver)
+        self.create_new_group(driver, name="", header="", footer="")
+        self.return_to_group_page(driver)
+        self.logout(driver)
+
+    def logout(self, driver):
+        driver.find_element(By.LINK_TEXT, "Logout").click()
+
+    def create_new_group(self, driver, name, header, footer):
         driver.find_element(By.NAME, "new").click()
         driver.find_element(By.NAME, "group_name").click()
         driver.find_element(By.NAME, "group_name").clear()
-        driver.find_element(By.NAME, "group_name").send_keys("12345")
+        driver.find_element(By.NAME, "group_name").send_keys(name)
         driver.find_element(By.NAME, "group_header").click()
         driver.find_element(By.NAME, "group_header").clear()
-        driver.find_element(By.NAME, "group_header").send_keys("rrrrrr")
+        driver.find_element(By.NAME, "group_header").send_keys(header)
         driver.find_element(By.NAME, "group_footer").click()
         driver.find_element(By.NAME, "group_footer").clear()
-        driver.find_element(By.NAME, "group_footer").send_keys("l;okljkhkh")
+        driver.find_element(By.NAME, "group_footer").send_keys(footer)
         driver.find_element(By.NAME, "submit").click()
+
+
+    def return_to_group_page(self, driver):
         driver.find_element(By.LINK_TEXT, "group page").click()
-        driver.find_element(By.LINK_TEXT, "Logout").click()
+
+    def open_group_page(self, driver):
+        driver.find_element(By.LINK_TEXT, "groups").click()
+
+    def login(self, driver, username, password):
+        driver.find_element(By.NAME, "user").click()
+        driver.find_element(By.NAME, "user").clear()
+        driver.find_element(By.NAME, "user").send_keys(username)
+        driver.find_element(By.ID, "LoginForm").click()
+        driver.find_element(By.NAME, "pass").click()
+        driver.find_element(By.NAME, "pass").clear()
+        driver.find_element(By.NAME, "pass").send_keys(password)
+        driver.find_element(By.XPATH, "//input[@value='Login']").click()
+
+    def open_home_page(self, driver):
+        driver.get("http://localhost/addressbook")
 
     def is_element_present(self, how, what):
         try:
