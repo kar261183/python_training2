3b874ea8cedb0781dc6420cd3a37336a0317b88a
Kar Tka <kar.tkachenko@gmail.com>
2022-11-09 20:29:33 -0800

    Create new group test

---
+++ test_2.py
@@ -0,0 +1,66 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest
+
+
+class UntitledTestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    def test_untitled_test_case(self):
+        driver = self.driver
+        driver.get("http://localhost/addressbook")
+        driver.find_element(By.NAME, "user").click()
+        driver.find_element(By.NAME, "user").clear()
+        driver.find_element(By.NAME, "user").send_keys("admin")
+        driver.find_element(By.ID, "LoginForm").click()
+        driver.find_element(By.NAME, "pass").click()
+        driver.find_element(By.NAME, "pass").clear()
+        driver.find_element(By.NAME, "pass").send_keys("secret")
+        driver.find_element(By.XPATH, "//input[@value='Login']").click()
+        driver.find_element(By.LINK_TEXT, "groups").click()
+        driver.find_element(By.NAME, "new").click()
+        driver.find_element(By.NAME, "group_name").click()
+        driver.find_element(By.NAME, "group_name").clear()
+        driver.find_element(By.NAME, "group_name").send_keys("njnkj")
+        driver.find_element(By.NAME, "group_header").click()
+        driver.find_element(By.NAME, "group_header").clear()
+        driver.find_element(By.NAME, "group_header").send_keys("iojiojijlj")
+        driver.find_element(By.NAME, "group_footer").click()
+        driver.find_element(By.NAME, "group_footer").clear()
+        driver.find_element(By.NAME, "group_footer").send_keys("ijijoijij")
+        driver.find_element(By.NAME, "submit").click()
+        driver.find_element(By.LINK_TEXT, "group page").click()
+        driver.find_element(By.LINK_TEXT, "Logout").click()
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
