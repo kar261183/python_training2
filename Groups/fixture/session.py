46eecbadf052b5f9f1a66e2db207983dd3cd5133
Kar Tka <kar.tkachenko@gmail.com>
2022-11-13 18:36:44 -0800

    smart login logout

--- Groups/fixture/session.py
+++ Groups/fixture/session.py
@@ -21,3 +21,25 @@ class SessionHelper:
     def logout(self):
         driver = self.app.driver
         driver.find_element(By.LINK_TEXT, "Logout").click()
+
+    def ensure_logout(self):
+        driver = self.app.driver
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        driver = self.app.driver
+        return len(driver.find_elements(By.LINK_TEXT, "Logout")) > 0
+
+    def ensure_login(self, username, password):
+        driver = self.app.driver
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
+
+    def is_logged_in_as(self, username):
+        driver = self.app.driver
+        return driver.find_element(By.XPATH, '//*[@id="top"]/form/b').text == "(" + username + ")"
